0b19ea4bc962694838c921d14abc7fcc71814648
Schweppie <joeppetersmail@gmail.com>
2020-12-01 20:14:57 +0100

    Fix tabs

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -2,19 +2,19 @@ data_file = open('../Data/Day1.txt')
 data_list = [int(i) for i in data_file.read().splitlines()]
 
 def Part1():
-	for i in data_list:
-		for j in data_list:
-			if (i + j) == 2020:
-				print(i * j)
-				return;
+    for i in data_list:
+        for j in data_list:
+            if (i + j) == 2020:
+                print(i * j)
+                return;
 
 def Part2():
-	for i in data_list:
-		for j in data_list:
-			for k in data_list:
-				if (i + j + k) == 2020:
-					print(i * j * k)
-					return;
+    for i in data_list:
+        for j in data_list:
+            for k in data_list:
+                if (i + j + k) == 2020:
+                    print(i * j * k)
+                    return;
 
 Part1()
 Part2()
