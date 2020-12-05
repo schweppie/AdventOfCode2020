56752bbf6931e6fab5598018f0e64b658a41bf33
Schweppie <joeppetersmail@gmail.com>
2020-12-05 08:56:58 +0100

    Fix Day4 part 2, small error

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -9,55 +9,40 @@ def SolvePart1(dataLines: str) -> int:
 
 def SolvePart2(dataLines: str) -> int:
     passwords = GetValidPasswords(dataLines)
-
-    passportList = []
-    passport = ""
-    dataLines.append("") # additional empty line eof
-    for line in dataLines:
-        if line == "":
-            passportList.append(passport)
-            passport = ""
-        else:
-            passport = passport + " " + line
-
-    validationTable =  [[r'(byr:)(19[2-9][0-9]|200[0-2])', 2], \
-                        [r'(iyr:)(201[0-9]|2020)', 2], \
-                        [r'(eyr:)(202[0-9]|2030)', 2], \
-                        [r'(hgt:)(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)', 2], \
-                        [r'(hcl:)(#[a-f0-9]{6})', 2], \
-                        [r'(ecl:)(amb|blu|brn|gry|grn|hzl|oth)', 2], \
-                        [r'(pid:)([0-9]{9}) ', 2]]
+    validationTable =  [[r'(^|\s)(byr:)(19[2-9][0-9]|200[0-2])(\s| $)', 4], \
+                        [r'(^|\s)(iyr:)(201[0-9]|2020)(\s| $)', 4], \
+                        [r'(^|\s)(eyr:)(202[0-9]|2030)(\s| $)', 4], \
+                        [r'(^|\s)(hgt:)(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)(\s| $)', 4], \
+                        [r'(^|\s)(hcl:)(#[a-f0-9]{6})(\s| $)', 4], \
+                        [r'(^|\s)(ecl:)(amb|blu|brn|gry|grn|hzl|oth)(\s| $)', 4], \
+                        [r'(^|\s)(pid:)([0-9]{9})(\s| $)', 4]]
     validPasswords = 0
-    for password in passportList:
+    for password in passwords:
         validations = 0
         for validation in validationTable:
             matches = re.search(validation[0], password)
             if matches:
                 if len(matches.groups()) == validation[1]:
                     validations = validations + 1
-        if validations == 7:
+        if validations == len(validationTable):
             validPasswords = validPasswords + 1
     return validPasswords
 
 def GetValidPasswords(dataLines: str) -> []:
     passportList = []
     passport = ""
-    dataLines.append("") # additional empty line eof
+    dataLines.append("") # additional empty line eof :/ ?
     for line in dataLines:
         if line == "":
             passportList.append(passport)
             passport = ""
         else:
-            passport = passport + " " + line
+            passport = passport + line + " "
     validPassportList = []
     for passport in passportList:
         if IsPasswordValid(passport):
             validPassportList.append(passport)
     return validPassportList
-
-
-#(\w+):(\d{4})
-
 
 def IsPasswordValid(password: str) -> bool:
     matches = re.findall(r'(\w+):(#|\w+)', password)
@@ -68,5 +53,5 @@ def IsPasswordValid(password: str) -> bool:
         return True
     return False
 
+print (SolvePart1(dataLines))
 print (SolvePart2(dataLines))
-##print (SolvePart2(data_lines))
