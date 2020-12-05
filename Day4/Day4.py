6adea4e3ed3e2cc851ce3c65ba455c5e1bef01d8
Schweppie <joeppetersmail@gmail.com>
2020-12-05 09:44:31 +0100

    Cleanup, used password instead of passport :D!

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -4,10 +4,10 @@ dataFile = open('../Data/Day4.txt')
 dataLines = [str(i) for i in dataFile.read().splitlines()]
 
 def SolvePart1(dataLines: str) -> int:
-    return len(GetValidPasswords(dataLines))
+    return len(GetValidPassports(dataLines))
 
 def SolvePart2(dataLines: str) -> int:
-    passwords = GetValidPasswords(dataLines)
+    passports = GetValidPassports(dataLines)
     validationTable =  [[r'(^|\s)(byr:)(19[2-9][0-9]|200[0-2])(\s| $)', 4], \
                         [r'(^|\s)(iyr:)(201[0-9]|2020)(\s| $)', 4], \
                         [r'(^|\s)(eyr:)(202[0-9]|2030)(\s| $)', 4], \
@@ -15,18 +15,18 @@ def SolvePart2(dataLines: str) -> int:
                         [r'(^|\s)(hcl:)(#[a-f0-9]{6})(\s| $)', 4], \
                         [r'(^|\s)(ecl:)(amb|blu|brn|gry|grn|hzl|oth)(\s| $)', 4], \
                         [r'(^|\s)(pid:)([0-9]{9})(\s| $)', 4]]
-    validPasswords = 0
-    for password in passwords:
+    validPassports = 0
+    for passport in passports:
         validations = 0
         for validation in validationTable:
-            matches = re.search(validation[0], password)
+            matches = re.search(validation[0], passport)
             if matches and len(matches.groups()) == validation[1]:
                 validations = validations + 1
         if validations == len(validationTable):
-            validPasswords = validPasswords + 1
-    return validPasswords
+            validPassports = validPassports + 1
+    return validPassports
 
-def GetValidPasswords(dataLines: str) -> []:
+def GetValidPassports(dataLines: str) -> []:
     passportList = []
     passport = ""
     dataLines.append("") # additional empty line eof :/ ?
@@ -38,15 +38,15 @@ def GetValidPasswords(dataLines: str) -> []:
             passport = passport + line + " "
     validPassportList = []
     for passport in passportList:
-        if IsPasswordValid(passport):
+        if IsPassportValid(passport):
             validPassportList.append(passport)
     return validPassportList
 
-def IsPasswordValid(password: str) -> bool:
-    matches = re.findall(r'(\w+):(#|\w+)', password)
+def IsPassportValid(passport: str) -> bool:
+    matches = re.findall(r'(\w+):(#|\w+)', passport)
     if len(matches) == 8:
         return True
-    match = re.search(r'(cid):', password)
+    match = re.search(r'(cid):', passport)
     if len(matches) == 7 and not match:
         return True
     return False
