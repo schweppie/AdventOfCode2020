2cbf80a6e63cccb3c8b11cfc0c413259da75e870
Schweppie <joeppetersmail@gmail.com>
2020-12-08 18:39:44 +0100

    Add day 8 solution

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -5,130 +5,80 @@ dataLines = [str(i) for i in dataFile.read().splitlines()]
 
 index = 0
 accumulator = 0
-instructions = []
-
-passedInstructionsDict = {}
-
-class NoOperation:
-    global index
-    def Execute(self):
-        index = index + 1
-    def Revert(self):
-        index = index - 1
-
-class JumpOperation:
-    global index
-    def __init__(self, jump: int):
-        self.jump = jump
-    def Execute(self):
-        index = index + self.jump
-    def Revert(self):
-        index = index - self.jump
-
-class AccumulateOperation:
-    global index
-    def __init__(self, jump: int):
-        self.jump = jump
-    def Execute(self):
-        index = index + self.jump
-    def Revert(self):
-        index = index - self.jump
-
-
-
-def test():
-
-    testList = [classa(), classb()]
-
-    for item in testList:
-        item.do()
-
-
-
-
-
-
-
 
 def SolvePart1(dataLines: str) -> int:
-    global index
+    passedInstructionsDict = {}
+    instructions = GetInstructions(dataLines)
     while True:
         if index in passedInstructionsDict:
             return accumulator
-        passedInstructionsDict[index] = 1
+        passedInstructionsDict[index] = passedInstructionsDict.get(index, 0) + 1
         instruction = instructions[index]
         method = eval(instruction[0])
-        method(instruction[1], False)
+        method(instruction[1])
     return 0
 
-def nop(amount: int, revert: bool):
-    global index
-    if revert:
-        index -= 1
-    else:
-        index += 1
-
-def acc(amount: int, revert: bool):
-    global index
-    global accumulator
-    if revert:
-        index -= 1
-        accumulator -= amount
-    else:
-        index += 1
-        accumulator += amount
-
-def jmp(amount: int, revert: bool):
-    global index
-    if revert:
-        index -= amount
-    else:
-        index += amount
-
-def SolvePart2(dataLines: str) -> int:
-    passedInstructionsDict = {}
+def GetOutputOf(instructions: []) -> []: # returns [ succes, accumulator]
     global index
     global accumulator
     index = 0
     accumulator = 0
+    passedInstructionsDict = {}
     while True:
-
         if index >= len(instructions):
-            return accumulator
-
+            return [True, accumulator]
+        passedInstructionsDict[index] = passedInstructionsDict.get(index, 0) + 1
+        if passedInstructionsDict[index] > 100:
+            return [False, 0]
         instruction = instructions[index]
-
-
-
-        oldIndex = index
-
         method = eval(instruction[0])
-        method(instruction[1], False)
+        method(instruction[1])
+    return [False, 0]
 
-        passedInstructionsDict[oldIndex] = 1
+def nop(amount: int):
+    global index
+    index += 1
 
-        print (str(index) + " inst: " + str(instruction) + " accu=" + str(accumulator) + " dict: " + str(passedInstructionsDict))
+def acc(amount: int):
+    global index
+    global accumulator
+    index += 1
+    accumulator += amount
 
-        x = input()
+def jmp(amount: int):
+    global index
+    index += amount
 
-        if index in passedInstructionsDict:
-            index = oldIndex
-            method(instruction[1], True)
-
-            print (str(index) + " REVERT inst: " + str(instruction) + " dict: " + str(passedInstructionsDict))
-            if instruction[0] == "nop":
-                instructions[index][0] = "jmp"
-            elif instruction[0] == "jmp":
-                instructions[index][0] = "nop"
-
-
+def SolvePart2(dataLines: str) -> int:
+    table = PopulateInstructionSolutions(dataLines)
+    for entry in table:
+        output = GetOutputOf(entry)
+        if output[0] == True:
+            return output[1]
     return 0
 
-def PopulateInstructions(dataLines: str):
+def GetInstructions(dataLines: str) -> []:
+    instructions = []
     for line in dataLines:
         match = re.match(r'(\w+) ([-+]\d+)', line)
         instructions.append([match.group(1), int(match.group(2))])
-test()
-PopulateInstructions(dataLines)
+    return instructions
+
+def PopulateInstructionSolutions(dataLines: str) -> []:
+    instructionListCollection = []
+    for i in range(len(dataLines)):
+        match = re.match(r'(\w+) ([-+]\d+)', dataLines[i])
+
+        if match.group(1) == "nop":
+            instructionList = GetInstructions(dataLines)
+            instructionList[i][0] = "jmp"
+            instructionListCollection.append(instructionList)
+
+        if match.group(1) == "jmp":
+            instructionList = GetInstructions(dataLines)
+            instructionList[i][0] = "nop"
+            instructionListCollection.append(instructionList)
+    return instructionListCollection
+
 print (SolvePart1(dataLines))
-#print (SolvePart2(dataLines))
+print (SolvePart2(dataLines))
